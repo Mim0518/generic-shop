89fd025e5561138e7831045f308f393f65b45da3
Guillermo Moreno Rivera <gmoreno@afdevelopment.com.mx>
2025-11-04 14:09:57 -0600

    Setup initial project structure with Angular framework. Added basic components, layouts, routes, and Tailwind CSS integration with DaisyUI.

---
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {ProductCard} from '../../components/product-card/product-card';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [
+    ProductCard
+  ],
+  templateUrl: './home-page.html',
+})
+export class HomePage {
+
+}
